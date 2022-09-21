a886bf129d1bd43ae77466f88071ac6787722d03
Allen Cohen <azc666@gmail.com>
2022-09-21 13:06:53 -0400

    Edit/delete tags

---
+++ resources/views/components/m-button.blade.php
@@ -0,0 +1,5 @@
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'ml-3 inline-flex justify-center rounded-md border
+  border-transparent bg-red-500 py-2 px-4 text-sm font-medium
+  text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2']) }}>
+  {{ $slot }}
+</button>
--- resources/views/livewire/tag-index.blade.php
+++ resources/views/livewire/tag-index.blade.php
@@ -8,33 +8,51 @@
                 <thead>
                     <tr
                         class="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
-                        <th class="px-4 py-3">Title</th>
-                        <th class="px-4 py-3">Date</th>
-                        <th class="px-4 py-3">Rating</th>
-                        <th class="px-4 py-3">Public</th>
+                        <th class="px-4 py-3">Name</th>
+                        <th class="px-4 py-3">Slug</th>
                         <th class="px-4 py-3">Manage</th>
                     </tr>
                 </thead>
                 <tbody class="bg-white">
+                    @forelse ($tags as $tag)
                     <tr class="text-gray-700">
                         <td class="px-4 py-3 border">
-                            Title here
+                            {{ $tag->tag_name }}
                         </td>
-                        <td class="px-4 py-3 text-ms font-semibold border">Date here</td>
-                        <td class="px-4 py-3 text-xs border">
-                            Rating here
+                        <td class="px-4 py-3 text-ms font-semibold border">{{ $tag->slug }}</td>
+                        <td class="px-4 py-3 text-sm border">
+                            <x-jet-button wire:click="showEditModal({{ $tag->id }})"
+                                class="bg-green-500 hover:bg-green-700 text-white">
+                                Edit
+                            </x-jet-button>
+                            <x-jet-button wire:click="deleteTag({{ $tag->id }})"
+                                class="bg-red-500 hover:bg-red-700 text-white">
+                                Delete
+                            </x-jet-button>
                         </td>
-                        <td class="px-4 py-3 text-sm border">Public</td>
+                    </tr>
+                    @empty
+                    <tr class="text-gray-700">
+                        <td class="px-4 py-3 border">
+                            Empty
+                        </td>
+                        <td class="px-4 py-3 text-ms font-semibold border">{{ $tag->slug }}</td>
                         <td class="px-4 py-3 text-sm border">
                             Edit/Delete
                         </td>
                     </tr>
+                    @endforelse
+
                 </tbody>
             </table>
         </div>
     </div>
     <x-jet-dialog-modal wire:model="showTagModal">
+        @if ($tagId)
+        <x-slot name="title"><span class="font-bold text-indigo-600 uppercase">Update Tag</span></x-slot>
+        @else
         <x-slot name="title"><span class="font-bold text-indigo-600 uppercase">Create Tag</span></x-slot>
+        @endif
 
         <x-slot name="content">
             <form class="space-y-6">
@@ -54,12 +72,16 @@
             </form>
         </x-slot>
         <x-slot name="footer">
-            <button wire:click="closeTagModal"
-                class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-red-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                Cancel</button>
-            <button wire:click="createTag"
-                class="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                Create</button>
+            <x-m-button wire:click="closeTagModal" class="">
+                Cancel</x-m-button>
+            @if ($tagId)
+            <x-m-button wire:click="updateTag" class="bg-blue-600 hover:bg-blue-800 text-white">
+                Update</x-m-button>
+            @else
+            <x-m-button wire:click="createTag" class="bg-blue-600 hover:bg-blue-800 text-white">
+                Create</x-m-button>
+            @endif
+
         </x-slot>
     </x-jet-dialog-modal>
 </section>
